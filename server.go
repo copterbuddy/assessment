4dc03b2d2c2d4a53e9984611281b7234da9d9e7c
Cop.Kunanon <copterbuddy@gmail.com>
2022-12-20 18:59:51 +0700

    add graceful shutdown

--- server.go
+++ server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"log"
+	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -13,7 +21,34 @@ func main() {
 
 	e := echo.New()
 
+	e.Logger.SetLevel(log.INFO)
+
 	e.Logger.Fatal(e.Start(":2565"))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// e.Logger.Fatal(e.Start(":2565"))
+	go func() {
+		if err := e.Start(":2565"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server: ", err)
+		}
+	}()
+
+	e.GET("/", func(c echo.Context) error {
+		time.Sleep(8 * time.Second)
+		return c.JSON(http.StatusOK, "OK")
+	})
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("bye bye")
 }
 
 var db *sql.DB
