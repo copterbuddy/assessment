3d0198e6cfe737c22a18ac890e6b246830cdd0ea
copterbuddy <copterbuddy@gmail.com>
2022-12-20 19:56:57 +0700

    use request method

--- expense/create_test.go
+++ expense/create_test.go
@@ -13,48 +13,36 @@ import (
 )
 
 func TestGetGreeting(t *testing.T) {
-	e := echo.New()
-	// req := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
-	// req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	// rec := httptest.NewRecorder()
-	// c := e.NewContext(req, rec)
-
-	c, rec := request(http.MethodGet, "/", strings.NewReader(""), e)
+	c, res := request(http.MethodGet, "/", strings.NewReader(""))
 
 	h := handler{}
 	err := h.Greeting(c)
 
 	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "Hello, World!", rec.Body.String())
+		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, "Hello, World!", res.Body.String())
 	}
 }
 
-func request(method, url string, body io.Reader, e *echo.Echo) (echo.Context, *httptest.ResponseRecorder) {
+func Test_Create_When_No_Request_Body(t *testing.T) {
+	c, res := request(http.MethodGet, "/", strings.NewReader(""))
+
+	h := handler{nil}
+	err := h.CreateExpenseHandler(c)
+	expected := "{\"message\":\"data incurrect\"}"
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+		assert.Equal(t, expected, strings.TrimSpace(res.Body.String()))
+	}
+}
+
+func request(method, url string, body io.Reader) (echo.Context, *httptest.ResponseRecorder) {
+	e := echo.New()
 	req := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 
 	c := e.NewContext(req, rec)
 	return c, rec
-}
-
-func Test_Create_When_No_Request_Body(t *testing.T) {
-
-	var err error
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/expenses", strings.NewReader(""))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-
-	h := handler{nil}
-	c := e.NewContext(req, rec)
-
-	err = h.CreateExpenseHandler(c)
-	expected := "{\"message\":\"data incurrect\"}"
-
-	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusBadRequest, rec.Code)
-		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
-	}
 }
