539e62836f36970df8d60bcb0ab4c35677e2848c
copterbuddy <copterbuddy@gmail.com>
2022-12-23 15:11:57 +0700

    add request test and converter
    This reverts commit 9214fcf01d97e2f9e47deb6185ff502a161ecc51.

---
+++ converter/converter.go
@@ -0,0 +1,18 @@
+package converter
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+)
+
+func ReqString(reqStruct interface{}) string {
+	if reqStruct == nil {
+		return ""
+	}
+	result, _ := json.Marshal(&reqStruct)
+	return string(result)
+}
+
+func ResStruct(res *httptest.ResponseRecorder, result interface{}) {
+	json.Unmarshal([]byte(res.Body.Bytes()), &result)
+}
