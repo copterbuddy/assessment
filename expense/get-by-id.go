e1a6090019668d3bfcc191bd73ed3787fb59b60d
copterbuddy <copterbuddy@gmail.com>
2022-12-25 15:06:34 +0700

    test get result as struct

--- expense/get-by-id.go
+++ expense/get-by-id.go
@@ -9,8 +9,16 @@ import (
 func (h *handler) GetExpenseByIdHandler(c echo.Context) error {
 	id := c.Param("id")
 	if id == "" {
-		return c.JSON(http.StatusBadRequest, Err{Message: "data incurrect"})
+		return c.JSON(http.StatusBadRequest, Err{Message: "data incorrect"})
 	}
 
-	return c.JSON(http.StatusOK, "ok")
+	result := Expense{
+		ID:     1,
+		Title:  "strawberry smoothie",
+		Amount: 79,
+		Note:   "night market promotion discount 10 bath",
+		Tags:   []string{"food", "beverage"},
+	}
+
+	return c.JSON(http.StatusOK, result)
 }
--- expense/get-by-id_test.go
+++ expense/get-by-id_test.go
@@ -8,29 +8,49 @@ import (
 	"testing"
 
 	_ "github.com/DATA-DOG/go-sqlmock"
+	"github.com/copterbuddy/assessment/converter"
 	"github.com/copterbuddy/assessment/request"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_Get_Expense_By_Id(t *testing.T) {
+	//Arrange
+	want := Expense{
+		ID:     1,
+		Title:  "strawberry smoothie",
+		Amount: 79,
+		Note:   "night market promotion discount 10 bath",
+		Tags:   []string{"food", "beverage"},
+	}
+
 	c, rec := request.Request(http.MethodGet, request.Uri("expenses"), "")
 	c.SetParamNames("id")
 	c.SetParamValues("1")
 
+	//Act
 	h := handler{}
 	err := h.GetExpenseByIdHandler(c)
 
+	ResponseBody := Expense{}
+	converter.ResStruct(rec, &ResponseBody)
+
+	//Assert
+	assert.NoError(t, err)
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, want, ResponseBody)
 	}
 }
 
 func Test_Get_Expense_By_Id_No_Param(t *testing.T) {
+	//Arrange
 	c, rec := request.Request(http.MethodGet, request.Uri("expenses"), "")
 
+	//Act
 	h := handler{}
 	err := h.GetExpenseByIdHandler(c)
 
+	//Assert
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusBadRequest, rec.Code)
 	}
