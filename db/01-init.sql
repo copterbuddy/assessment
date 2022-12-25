5fc7cded494bcd1af99d8bc798702f382f5ab6b1
copterbuddy <copterbuddy@gmail.com>
2022-12-25 17:20:19 +0700

    add it test get by id

--- db/01-init.sql
+++ db/01-init.sql
@@ -10,3 +10,5 @@ CREATE TABLE IF NOT EXISTS expenses (
     "tags" TEXT[],
     PRIMARY KEY ("id")
 );
+
+INSERT INTO "expenses" ("title","amount","note","tags") VALUES ('strawberry smoothie', 79, 'night market promotion discount 10 bath', '{"food", "beverage"}');
