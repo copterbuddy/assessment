7811df7015de9fe2b84521ce04932064cde8a87a
copterbuddy <copterbuddy@gmail.com>
2022-12-25 16:49:36 +0700

    add unit test not found

--- expense/get-by-id.go
+++ expense/get-by-id.go
@@ -15,10 +15,15 @@ func (h *handler) GetExpenseByIdHandler(c echo.Context) error {
 	}
 
 	result := Expense{}
+
 	row := h.DB.QueryRow("SELECT * FROM expenses WHERE id = $1", id)
 	switch err := row.Scan(&result.ID, &result.Title, &result.Amount, &result.Note, pq.Array(&result.Tags)); err {
 	case sql.ErrNoRows:
-		return c.JSON(http.StatusBadRequest, Err{Message: "Not found youe expense"})
+		return c.JSON(http.StatusBadRequest, Err{Message: "Not found your expense"})
+	}
+
+	if result.ID == 0 {
+		return c.JSON(http.StatusBadRequest, Err{Message: "Not found your expense"})
 	}
 
 	return c.JSON(http.StatusOK, result)
--- expense/get-by-id_test.go
+++ expense/get-by-id_test.go
@@ -4,6 +4,7 @@
 package expense
 
 import (
+	"database/sql"
 	"net/http"
 	"testing"
 
@@ -26,7 +27,7 @@ func Test_Get_Expense_By_Id(t *testing.T) {
 
 	c, rec := request.Request(http.MethodGet, request.Uri("expenses"), "")
 	c.SetParamNames("id")
-	c.SetParamValues("1")
+	c.SetParamValues("2")
 
 	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
 		AddRow(want.ID, want.Title, want.Amount, want.Note, pq.Array(want.Tags))
@@ -48,6 +49,76 @@ func Test_Get_Expense_By_Id(t *testing.T) {
 	assert.NoError(t, err)
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, want, ResponseBody)
+	}
+}
+
+func Test_Get_Expense_By_Id_Error_No_Row(t *testing.T) {
+	//Arrange
+	want := Err{
+		Message: "Not found your expense",
+	}
+
+	c, rec := request.Request(http.MethodGet, request.Uri("expenses"), "")
+	c.SetParamNames("id")
+	c.SetParamValues("1")
+
+	newsMockRows := sql.ErrNoRows
+	db, mock, err := sqlmock.New()
+	mock.ExpectQuery("SELECT (.+) FROM expenses").WillReturnError(newsMockRows)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	h := handler{db}
+
+	//Act
+	err = h.GetExpenseByIdHandler(c)
+
+	ResponseBody := Err{}
+	converter.ResStruct(rec, &ResponseBody)
+
+	//Assert
+	assert.NoError(t, err)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, want, ResponseBody)
+	}
+}
+
+func Test_Get_Expense_By_Id_Not_Found(t *testing.T) {
+	//Arrange
+	want := Err{Message: "Not found your expense"}
+	c, rec := request.Request(http.MethodGet, request.Uri("expenses"), "")
+	c.SetParamNames("id")
+	c.SetParamValues("2")
+
+	expenseMock := Expense{
+		ID:     0,
+		Title:  "",
+		Amount: 0,
+		Note:   "",
+		Tags:   nil,
+	}
+	newsMockRows := sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
+		AddRow(expenseMock.ID, expenseMock.Title, expenseMock.Amount, expenseMock.Note, pq.Array(expenseMock.Tags))
+
+	db, mock, err := sqlmock.New()
+	mock.ExpectQuery("SELECT (.+) FROM expenses").WillReturnRows(newsMockRows)
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	h := handler{db}
+
+	//Act
+	err = h.GetExpenseByIdHandler(c)
+
+	ResponseBody := Err{}
+	converter.ResStruct(rec, &ResponseBody)
+
+	//Assert
+	assert.NoError(t, err)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
 		assert.Equal(t, want, ResponseBody)
 	}
 }
