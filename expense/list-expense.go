bde372d3424990749dd6dea34a8c7ae4fd3913ef
copterbuddy <copterbuddy@gmail.com>
2022-12-26 19:51:56 +0700

    add endpoint list expense

--- expense/list-expense.go
+++ expense/list-expense.go
@@ -1,1 +1,11 @@
 package expense
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (h *handler) ListExpenseHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, "ok")
+}
--- expense/list-expense_test.go
+++ expense/list-expense_test.go
@@ -1,1 +1,24 @@
 package expense
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/copterbuddy/assessment/request"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Get_Expense_By_Id(t *testing.T) {
+	//Arrange
+	c, rec := request.Request(http.MethodGet, request.Uri("expenses"), "")
+	h := handler{nil}
+
+	//Act
+	err := h.ListExpenseHandler(c)
+
+	//Assert
+	assert.NoError(t, err)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
