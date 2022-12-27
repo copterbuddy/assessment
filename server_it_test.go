2f54ea1b8682f9d5f2057d079e138f1af49ba17f
copterbuddy <copterbuddy@gmail.com>
2022-12-27 10:32:19 +0700

    add no auth it test

---
+++ server_it_test.go
@@ -0,0 +1,74 @@
+//go:build integration
+// +build integration
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/copterbuddy/assessment/expense"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+const serverPort = 2565
+
+func Test_it_No_Auth(t *testing.T) {
+
+	eh := echo.New()
+	eh.Use(Auth)
+	go func(e *echo.Echo) {
+		h := expense.NewExpenseHandler(nil)
+
+		e.GET("/expenses", h.GetExpenseByIdHandler)
+		e.Start(fmt.Sprintf(":%d", serverPort))
+	}(eh)
+	for {
+		conn, err := net.DialTimeout("tcp", fmt.Sprintf("localhost:%d", serverPort), 30*time.Second)
+		if err != nil {
+			log.Println(err)
+		}
+		if conn != nil {
+			conn.Close()
+			break
+		}
+	}
+
+	//Arrange
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/expenses", serverPort), strings.NewReader(""))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set(echo.HeaderAuthorization, "November 10, 2009wrong_token")
+	client := http.Client{}
+
+	//Act
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	var result string
+	json.Unmarshal(byteBody, &result)
+
+	//Assert
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+		assert.Equal(t, "You are not authorized!", result)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = eh.Shutdown(ctx)
+	assert.NoError(t, err)
+}
